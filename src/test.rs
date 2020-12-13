607d6a01ed61b33793ee4cb9fde4c4ac85df7f79
rachel-rose <ihateface@autistici.org>
2020-12-13 21:25:35 +0100

    working version

--- src/test.rs
+++ src/test.rs
@@ -1,27 +1,44 @@
-use async_executor::Executor;
+use smol::{future, io, Timer};
+use std::thread;
+use std::time::Duration;
+use async_channel::unbounded;
 use easy_parallel::Parallel;
-use futures_lite::future;
+use async_executor::Executor;
 
 async fn sleep(dur: Duration) {
     Timer::after(dur).await;
 }
 
-async fn hello_loop() {
+async fn foo() {
     loop {
         println!("Hello fren");
         sleep(Duration::from_secs(2)).await;
     }
 }
 
-async fn pingpong(ex: Executor<'_>)  -> io::Result<()> {
+async fn bar() {
+    loop {
+        println!("fren");
+        sleep(Duration::from_secs(1)).await;
+    }
+}
+    
+async fn pingpong(ex: &Executor<'_>)  -> io::Result<()> {
     // spawn hello loop in parallel
     ex.spawn(async {
-        hello_loop().await;
+        foo().await;
     })
     .detach();
-
+    ex.spawn(async {
+        bar().await;
+    })
+    .detach();
+    println!("Debug 1");
+    thread::sleep(Duration::from_secs(5));
+    println!("Debug 2");
     Ok(())
 }
+
 
 fn main() -> io::Result<()> {
     let ex = Executor::new();
@@ -32,10 +49,9 @@ Parallel::new()
     .each(0..4, |_| future::block_on(ex.run(shutdown.recv())))
     // Run the main future on the current thread.
     .finish(|| future::block_on(async {
-        pingpong(ex).await;
+        pingpong(&ex).await;
         drop(signal);
     }));
 
-    thread::sleep(Duration::from_secs(5));
     Ok(())
 }     
